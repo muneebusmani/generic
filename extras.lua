e9a8dc24d5641845e355ec6d8cdcea72043c8e17
muneebusmani <muneebusmani8355@gmail.com>
2024-06-06 22:02:31 +0500

    -m

--- extras.lua
+++ extras.lua
@@ -5,6 +5,7 @@ return {
   { import = "lazyvim.plugins.extras.lsp.none-ls" },
   { import = "lazyvim.plugins.extras.lsp.neoconf" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
   { import = "lazyvim.plugins.extras.formatting.prettier" },
   { import = "lazyvim.plugins.extras.linting.eslint" },
   -- { import = "lazyvim.plugins.extras.coding.mini-surround" },

4df00aac8d2d4390272b9ef237a495f10a17b3fd
muneebusmani <muneebusmani8355@gmail.com>
2024-06-06 05:03:17 +0500

    updates

--- extras.lua
+++ extras.lua
@@ -1,10 +1,9 @@
 return {
   { import = "lazyvim.plugins.extras.ui.edgy" },
-  { import = "lazyvim.plugins.extras.lsp.none-ls" },
   { import = "lazyvim.plugins.extras.editor.harpoon2" },
   { import = "lazyvim.plugins.extras.editor.outline" },
+  { import = "lazyvim.plugins.extras.lsp.none-ls" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
   -- { import = "lazyvim.plugins.extras.coding.mini-surround" },
   -- { import = "lazyvim.plugins.extras.coding.luasnip" },
-
 }
-
